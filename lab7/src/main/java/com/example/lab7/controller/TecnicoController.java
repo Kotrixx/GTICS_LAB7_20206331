ead476776dc46f8629cadbe894c4c0ecf5eecae0
Kotrix <a20206331@pucp.edu.pe>
2023-09-28 21:09:23 -0500

    parte del formulario creado

--- lab7/src/main/java/com/example/lab7/controller/TecnicoController.java
+++ lab7/src/main/java/com/example/lab7/controller/TecnicoController.java
@@ -1,11 +1,16 @@
 package com.example.lab7.controller;
 
 
+import com.example.lab7.entity.Technician;
 import com.example.lab7.repository.TecnicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping(value = "/tecnico")
@@ -18,5 +23,20 @@ public class TecnicoController {
     public String listarTecnicos(Model model){
         model.addAttribute("listaTecnicos",tecnicoRepository.findAll());
         return "tecnicos/lista";
+    }
+
+    @RequestMapping(value = "/edit")
+    public String listarTecnicos(@ModelAttribute("product") Technician technician,
+                                 Model model, @RequestParam("id") int id){
+
+        Optional<Technician> optionalTechnician = tecnicoRepository.findById(id);
+        if (optionalTechnician.isPresent()) {
+            technician = optionalTechnician.get();
+            model.addAttribute("tecnico", technician);
+            return "tecnicos/form";
+        } else {
+            return "tecnicos/lista";
+        }
+
     }
 }
